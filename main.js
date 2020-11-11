766215c4c6562bf3d7b1fd09f529a650a2aaec98
Marco <markoka1@outlook.it>
2020-11-11 19:46:48 +0100

    Aggiunta commenti

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ var app = new Vue({
         },
         methods: {
             add_item(){
+                // Se l'elemento tracciato via input in nuovo_item è diverso
+                // da una stringa vuota, push del suo contenuto nella lista.
                 if(this.nuovo_item != ""){
                     this.list.push(this.nuovo_item);
+                    //Clear di nuovo_item.
                     this.nuovo_item = "";
                 }
             },
             del_item(index){
+                //Via 'splice(index,n)' rimuovo n to-do da 'list[]'.
                 this.list.splice(index, 1);
             }
         }
