46536c55aee16c90754e6b66f0e9c5a7bacc2a8f
Marco <markoka1@outlook.it>
2021-02-05 17:19:57 +0100

    fix bug

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 var app = new Vue({
     el: '#root',
     data: {
+        isActive: false,
         titolo: "TODOLIST!",
-        list: ["Sveglia ore 8:00a.m.","Scrivere codice","Andare a correre","Fare la spesa","Riposare"],
+        list: [
+
+            {
+                todo: "Sveglia ore 8:00a.m.",
+                check_i: false
+            },
+            {
+                todo: "Andare a correre.",
+                check_i: false
+            },
+            {
+                todo: "Fare colazione.",
+                check_i: false
+            },
+            {
+                todo: "Visitare un museo.",
+                check_i: false
+            },
+            {
+                todo: "Leggere.",
+                check_i: false
+            },
+        ],
         nuovo_item: ""
         },
         methods: {
-            add_item(){
+            add_item(element){
                 // Se l'elemento tracciato via input in nuovo_item è diverso
                 // da una stringa vuota, push del suo contenuto nella lista.
-                if(this.nuovo_item != ""){
-                    this.list.push(this.nuovo_item);
-                    //Clear di nuovo_item.
+                if(element != ""){
+                    this.list.push({
+                        todo: element,
+                        check_i: false
+                    })
+
                     this.nuovo_item = "";
                 }
             },
             del_item(index){
                 //Via 'splice(index,n)' rimuovo n to-do da 'list[]'.
                 this.list.splice(index, 1);
+            },
+            check_item(element){
+                if(element.check_i == false){
+                    element.check_i = true;
+                } else {
+                    element.check_i = false;
+                }
             }
         }
     });
